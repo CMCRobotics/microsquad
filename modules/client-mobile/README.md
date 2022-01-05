ae4f317ed08103716ea4fcbe83a122f5a55f8b2b
bcopy <500789+bcopy@users.noreply.github.com>
2022-01-05 21:19:53 +0100

    Add popmotion

--- modules/client-mobile/README.md
+++ modules/client-mobile/README.md
@@ -1,4 +1,14 @@
-# 
+# Microsquad Client
+
+A mobile client for MicroSquad.
+
+The client integrates a shared game status view (typically an iframe), as well as Microsquad interaction panels to support typical group interactions (display images or texts, participate to votes, read sensor values etc...).
+
+The client uses :
+* Vue (PWA)
+* Buefy (Bulma UI framework)
+
+The UI can be themed via SASS.
 
 ## Project setup
 ```
@@ -24,6 +34,3 @@ npm run test:unit
 ```
 npm run lint
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- modules/client-mobile/src/App.vue
+++ modules/client-mobile/src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <section class="hero is-danger is-fullheight">
+      <div class="hero-body">
+        <div class="">
+          <p class="title">Fullheight hero</p>
+          <p class="subtitle">Fullheight subtitle</p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+
+Vue.use(Buefy);
 
 @Component({
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 })
 export default class App extends Vue {}
@@ -24,6 +37,6 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
 }
 </style>
