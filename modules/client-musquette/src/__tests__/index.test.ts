bea78c5e47d84c88de11bf72985f218c5aee9054
bcopy <500789+bcopy@users.noreply.github.com>
2023-01-12 19:05:06 +0100

    Initial commit

--- modules/client-musquette/src/__tests__/index.test.ts
+++ modules/client-musquette/src/__tests__/index.test.ts
@@ -1,5 +1,20 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, afterAll } from "vitest";
 import { foobar } from "../index";
+import { of,from } from 'rxjs';
+
+const aedes = require('aedes')()
+const httpServer = require('http').createServer()
+const ws = require('websocket-stream')
+const port = 8888
+httpServer.listen(port, function () {
+  console.log('websocket server listening on port ', port)
+})
+
+afterAll(() => {
+  httpServer.close();
+  aedes.close();
+});
+
 
 describe("foobar()", () => {
   describe("given two positive integers", () => {
@@ -9,6 +24,14 @@ describe("foobar()", () => {
     describe("when called", () => {
       it("returns the sum of them multiplied by 3", () => {
         expect(foobar(first, second)).toEqual(9);
+      });
+    });
+
+    describe('test subscription', () => {
+      it("checks that hello is received", () => {
+          of('hello').subscribe( data => {
+          expect(data).toBe('hello');
+        });
       });
     });
   });
